68040f4c1ce7eed558946de199c4c5564f2c9b81
inuitwallet <mj2p.sg@gmail.com>
2019-11-08 15:32:45 +0000

    handle no responses

--- price_aggregator/management/commands/calculate_aggregates.py
+++ price_aggregator/management/commands/calculate_aggregates.py
@@ -16,6 +16,10 @@ class Command(BaseCommand):
     @staticmethod
     def remove_outliers(responses):
         ys = [float(resp.value) for resp in responses]
+
+        if not ys:
+            return responses
+
         quartile_1, quartile_3 = np.percentile(ys, [25, 75])
         iqr = quartile_3 - quartile_1
         lower_bound = quartile_1 - (iqr * 1.5)
