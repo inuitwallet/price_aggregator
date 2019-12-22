56e5454e648f740c2e25e0860677c7380e62d759
inuitwallet <mj2p.sg@gmail.com>
2019-12-22 09:35:13 +0000

    remove exchange providers. remove volume from coinmaketcap

--- price_aggregator/providers/coinmarketcappro.py
+++ price_aggregator/providers/coinmarketcappro.py
@@ -53,8 +53,7 @@ class CoinMarketCapPro(object):
                     output.append(
                         {
                             'coin': currency,
-                            'price': Decimal(coin_data[coin].get('quote', {}).get('USD', {}).get('price', 0)),
-                            'volume': Decimal(coin_data[coin].get('quote', {}).get('USD', {}).get('volume_24h', 0))
+                            'price': Decimal(coin_data[coin].get('quote', {}).get('USD', {}).get('price', 0))
                         }
                     )
 
