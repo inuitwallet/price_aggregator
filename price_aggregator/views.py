dc93180268f9cd56c8979623087dd67e5428c6c6
inuitwallet <mj2p.sg@gmail.com>
2019-12-26 22:16:45 +0000

    only show currency providers if full is requestsed

--- price_aggregator/views.py
+++ price_aggregator/views.py
@@ -18,49 +18,47 @@ class IndexView(View):
         request_url = '{}://{}'.format(request.META.get('HTTP_X_FORWARDED_PROTO', 'http'), request.get_host())
         return JsonResponse(
             {
-                '1_site_name': 'Nu Price Aggregator',
-                '2_site_function': 'Collect prices of Currencies from various sources and aggregate them. '
-                                   'The aim is to create a more stable price source that is not reliant on any '
-                                   'single provider. Prices are displayed in USD',
-                '3_endpoints': [
-                    {
-                        'url': '{}/currencies'.format(request_url),
-                        'url_function': 'Display all currencies that have current aggregated prices'
-                    },
-                    {
-                        'url': '{}/providers'.format(request_url),
-                        'url_function': 'Display all active providers and the currencies they provide information for'
-                    },
-                    {
-                        'url': '{}/price/<currency_code>'.format(request_url),
-                        'url_function': 'Display aggregated price data for the currency specified by <currency_code>'
-                    },
-                    {
-                        'url': '{}/price/<currency_code>/<date_time>'.format(request_url),
-                        'url_function': 'Display aggregated price data for the currency specified by <currency_code> '
-                                        'at the date_time given by <date_time> (yyyy-mm-ddTHH:MM:SS)'
-                    },
-                    {
-                        'url': '{}/movement/<currency_code>'.format(request_url),
-                        'url_function': 'Display the price movement over a range of times'
-                    },
-                    {
-                        'url': '{}/provider/<provider>'.format(request_url),
-                        'url_function': 'Display historical responses from a given provider'
-                    },
-                    {
-                        'url': '{}/provider/<provider>/price/<currency_code>'.format(request_url),
-                        'url_function': 'get the latest and moving average prices from a single provider'
-                    },
-                    {
-                        'url': '{}/provider/<provider>/price/<currency_code>/<date_time>'.format(request_url),
-                        'url_function': 'Display single provider data for the currency specified by <currency_code> '
-                                        'at the date_time given by <date_time> (yyyy-mm-ddTHH:MM:SS)'
-                    }
-                ]
-            },
-            json_dumps_params={
-                'sort_keys': True
+                'Nu Price Aggregator': {
+                    'Description': 'Collect prices of Currencies from various sources and aggregate them. '
+                    'The aim is to create a more stable price source that is not reliant on any '
+                    'single provider. Prices are displayed in USD',
+                    'Endpoints': [
+                        {
+                            'url': '{}/currencies'.format(request_url),
+                            'url_function': 'Display all currencies that have current aggregated prices'
+                        },
+                        {
+                            'url': '{}/providers'.format(request_url),
+                            'url_function': 'Display all active providers and the currencies they provide information for'
+                        },
+                        {
+                            'url': '{}/price/<currency_code>'.format(request_url),
+                            'url_function': 'Display aggregated price data for the currency specified by <currency_code>'
+                        },
+                        {
+                            'url': '{}/price/<currency_code>/<date_time>'.format(request_url),
+                            'url_function': 'Display aggregated price data for the currency specified by <currency_code> '
+                                            'at the date_time given by <date_time> (yyyy-mm-ddTHH:MM:SS)'
+                        },
+                        {
+                            'url': '{}/movement/<currency_code>'.format(request_url),
+                            'url_function': 'Display the price movement over a range of times'
+                        },
+                        {
+                            'url': '{}/provider/<provider>'.format(request_url),
+                            'url_function': 'Display historical responses from a given provider'
+                        },
+                        {
+                            'url': '{}/provider/<provider>/price/<currency_code>'.format(request_url),
+                            'url_function': 'get the latest and moving average prices from a single provider'
+                        },
+                        {
+                            'url': '{}/provider/<provider>/price/<currency_code>/<date_time>'.format(request_url),
+                            'url_function': 'Display single provider data for the currency specified by <currency_code> '
+                                            'at the date_time given by <date_time> (yyyy-mm-ddTHH:MM:SS)'
+                        }
+                    ]
+                }
             }
         )
 
@@ -164,13 +162,16 @@ class CurrenciesView(View):
         currencies = Currency.objects.all()
 
         for currency in currencies:
-            supported_providers = ProviderResponse.objects.filter(
-                currency=currency,
-                date_time__gte=now() - datetime.timedelta(days=1)
-            ).values_list(
-                'provider__name',
-                flat=True
-            )
+            if 'full' in request.GET:
+                supported_providers = ProviderResponse.objects.filter(
+                    currency=currency,
+                    date_time__gte=now() - datetime.timedelta(days=1)
+                ).values_list(
+                    'provider__name',
+                    flat=True
+                )
+            else:
+                supported_providers = []
 
             response[currency.code] = list(set(supported_providers))
 
